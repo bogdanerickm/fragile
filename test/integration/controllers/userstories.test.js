aaab6d04a049cc9447cf93d0d9e2740ee29ef0e6
Pablo <pablopissi@gmail.com>
2019-05-28 00:06:52 -0300

    Cambio yield por return en los tests que faltaban

--- test/integration/controllers/userstories.test.js
+++ test/integration/controllers/userstories.test.js
@@ -1,5 +1,7 @@
+/* eslint-disable require-yield */
 const strapi = require("strapi");
 const request = require("co-supertest");
+var assert = require('assert');
 
 describe("userStory Controller Integration", function() {
   describe("GET /userstories", function() {
@@ -27,7 +29,7 @@ describe("userStory Controller Integration", function() {
 
   describe("GET /userstories/:id", function() {
     it("should return 200 status code", function*() {
-      yield request("http://localhost:1337")
+      return request("http://localhost:1337")
         .get("/userstories/3")
         .expect(200)
         .end();
@@ -36,7 +38,7 @@ describe("userStory Controller Integration", function() {
 
   describe("GET /userstories/count", function() {
     it("should return 200 status code", function*() {
-      yield request("http://localhost:1337")
+      return request("http://localhost:1337")
         .get("/userstories/count")
         .expect(200)
         .end();
